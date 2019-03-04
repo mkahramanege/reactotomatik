74b9843b17afda5ade0d28ef356f88bd80c777ae
Mustafa Kahraman <mkahramanege@hotmail.com>
2019-03-04 10:29:46 +0300

    App.js düzenlendi.

--- App.js
+++ App.js
@@ -1,30 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import codePush from 'react-native-code-push';
 
 type Props = {};
 export default class App extends Component<Props> {
+  guncelle(){
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Button title='Uygulama Güncelle' onPress={this.guncelle()}/>
       </View>
     );
   }
